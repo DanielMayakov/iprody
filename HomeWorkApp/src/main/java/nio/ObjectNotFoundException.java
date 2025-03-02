9c832592705ff9b0b4fec7c39ee77617ff810c6d
danil <danil.mayakov@yandex.ru>
2025-03-02 15:35:43 +0300

    Внёс исправления

--- HomeWorkApp/src/main/java/nio/ObjectNotFoundException.java
+++ HomeWorkApp/src/main/java/nio/ObjectNotFoundException.java
@@ -1,6 +1,6 @@
 package nio;
 
-public class ObjectNotFoundException extends Exception {
+public class ObjectNotFoundException extends RuntimeException {
   public ObjectNotFoundException(String message) {
     super(message);
   }
